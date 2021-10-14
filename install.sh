260a87b5764b8513e5f0983ed27d4c980ff3c2e1
Mark Andrew Acosta Jamito <64141186+Mario10100MC@users.noreply.github.com>
2021-10-14 20:01:07 +0800

    Update To Install.sh
    Installs The Plasmoid into the /usr/share/plasma/plasmoids/ instead of the user directory

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-#TODO
+#!/usr/bin/bash
+git clone https://github.com/Pear-Project/pearOS-Default-Launchpad
+cd pearOS-Default-Launchpad
+/usr/bin/mkdir -p /usr/share/plasma/plasmoids/pearOSLaunchpad
+/usr/bin/cp -r ./ /usr/share/plasma/plasmoids/pearOSLaunchpad/
